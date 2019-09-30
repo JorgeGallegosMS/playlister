e4c077e6f6132933c1141ac4c04ce2775d1e1ded
Jorge Gallegos <gallegosjorge403@gmail.com>
2019-09-29 23:32:22 -0700

    Added production database

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
-client = MongoClient()
-db = client.Playlister
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
+client = MongoClient(host=host)
+db = client.get_default_database()
 # db.playlists.drop()
 playlists = db.playlists
 
