02c6ad4f2549d365964573bca7f18abce01ff4c4
Jorge Gallegos <gallegosjorge403@gmail.com>
2019-09-29 23:42:41 -0700

    Fixed MongoClient error

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
-client = MongoClient(host=host)
+client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 # db.playlists.drop()
 playlists = db.playlists
@@ -78,4 +78,4 @@ def playlists_delete(playlist_id):
     return redirect(url_for('playlists_index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
